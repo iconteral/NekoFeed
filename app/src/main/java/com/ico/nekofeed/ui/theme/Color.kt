2618c35a427c7ecc7dad0564aa07f0b8b4feb1cb
iconteral <iconteral@yahoo.com>
2026-06-06 18:11:20 +0800

    New: colour system

--- app/src/main/java/com/ico/nekofeed/ui/theme/Color.kt
+++ app/src/main/java/com/ico/nekofeed/ui/theme/Color.kt
@@ -2,53 +2,98 @@ package com.ico.nekofeed.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Primary - 蓝紫色主色调 (Committed策略)
-val Primary = Color(0xFF5C4DFF)
-val PrimaryDark = Color(0xFF4A3DE0)
-val PrimaryLight = Color(0xFF8B7FFF)
-val PrimarySoft = Color(0xFFE8E7FF)  // Solid light blue-purple
-val PrimaryGlow = Color(0x405C4DFF) // 25% opacity
+// Primary - Sea Blue 主交互色
+// 注意：#8CC0EB 太浅，所以 Primary 用加深版
+val Primary = Color(0xFF4A90C9)
+val PrimaryDark = Color(0xFF2F6FA3)
+val PrimaryLight = Color(0xFF8CC0EB)
+val PrimarySoft = Color(0xFFEAF6FF)
+val PrimaryGlow = Color(0x334A90C9)
 
-// Secondary - 紫罗兰
-val Secondary = Color(0xFF8B5CF6)
-val SecondaryDark = Color(0xFF7C3AED)
-val SecondaryLight = Color(0xFFA78BFA)
+// Secondary - Cream Yellow 品牌点缀
+val Secondary = Color(0xFFFFD66B)
+val SecondaryDark = Color(0xFFE7A900)
+val SecondaryLight = Color(0xFFFFF9D2)
+val SecondarySoft = Color(0xFFFFF7D8)
+
+// Tertiary - Almond Beige 柔和辅助色
+val Tertiary = Color(0xFFE9B96E)
+val TertiaryDark = Color(0xFFC58A32)
+val TertiaryLight = Color(0xFFFFEBCC)
+val TertiarySoft = Color(0xFFFFF3E2)
 
 // Accent Colors
-val AccentRed = Color(0xFFFF3B30)
-val AccentOrange = Color(0xFFFF9F0A)
-val AccentGreen = Color(0xFF30D158)
-val AccentBlue = Color(0xFF007AFF)
-val AccentPink = Color(0xFFFF2D55)
+val AccentRed = Color(0xFFE5484D)
+val AccentOrange = Color(0xFFFFA726)
+val AccentGreen = Color(0xFF2FBF71)
+val AccentBlue = Color(0xFF4A90C9)
+val AccentPink = Color(0xFFE85D9E)
 
-// Light Theme Neutrals
-val LightBackground = Color(0xFFF4F5F9)
+// Light Theme Neutrals - 不要全屏黄，背景要接近白
+val LightBackground = Color(0xFFFFFEF7)
 val LightSurface = Color(0xFFFFFFFF)
-val LightSurfaceVariant = Color(0xFFECEDF3)
-val LightOnBackground = Color(0xFF1C1C1E)
-val LightOnSurface = Color(0xFF1C1C1E)
-val LightOnSurfaceVariant = Color(0xFF3A3A3C)
-val LightTextMuted = Color(0xFF8E8E93)
+val LightSurfaceVariant = Color(0xFFF3F8FC)
+
+val LightOnBackground = Color(0xFF1F2933)
+val LightOnSurface = Color(0xFF1F2933)
+val LightOnSurfaceVariant = Color(0xFF5F6F7A)
+val LightTextMuted = Color(0xFF8A9AA5)
+
+// Containers
+val BlueContainer = Color(0xFFEAF6FF)
+val CreamContainer = Color(0xFFFFF9D2)
+val BeigeContainer = Color(0xFFFFEBCC)
+val SkyContainer = Color(0xFFBFDDF0)
+
+val Outline = Color(0xFFDCE8F0)
+val DividerColor = Color(0xFFE7EEF3)
+
+// Chart Colors
+val ChartLine = Color(0xFF4A90C9)
+val ChartLineSoft = Color(0xFFEAF6FF)
+
+// Segment Button Colors
+val SegmentSelected = Color(0xFFEAF6FF)
+val SegmentBorder = Color(0xFFDCE8F0)
+val SegmentTextSelected = Color(0xFF4A90C9)
+val SegmentTextNormal = Color(0xFF1F2933)
+
+// Rank Colors
+val RankOne = Color(0xFFE9B96E)
+val RankTwo = Color(0xFF8CC0EB)
+val RankThree = Color(0xFFBFDDF0)
 
 // Dark Theme Neutrals
-val DarkBackground = Color(0xFF0A0A0C)
-val DarkSurface = Color(0xFF1C1C1E)
-val DarkSurfaceVariant = Color(0xFF141416)
-val DarkOnBackground = Color(0xFFF5F5F7)
-val DarkOnSurface = Color(0xFFF5F5F7)
-val DarkOnSurfaceVariant = Color(0xFFD1D1D6)
-val DarkTextMuted = Color(0xFF636366)
+val DarkBackground = Color(0xFF0D1117)
+val DarkSurface = Color(0xFF161B22)
+val DarkSurfaceVariant = Color(0xFF1F2933)
+val DarkOnBackground = Color(0xFFF5F7FA)
+val DarkOnSurface = Color(0xFFF5F7FA)
+val DarkOnSurfaceVariant = Color(0xFFD0D7DE)
+val DarkTextMuted = Color(0xFF8B949E)
 
-// Stat Card Gradients (for StatsScreen)
-val StatPurpleStart = Color(0xFF5C4DFF)
-val StatPurpleEnd = Color(0xFF8B5CF6)
-val StatRedStart = Color(0xFFFF3B30)
-val StatRedEnd = Color(0xFFFF6B6B)
-val StatOrangeStart = Color(0xFFFF9F0A)
-val StatOrangeEnd = Color(0xFFFFCC02)
-val StatGreenStart = Color(0xFF30D158)
-val StatGreenEnd = Color(0xFF34C759)
-val StatBlueStart = Color(0xFF007AFF)
-val StatBlueEnd = Color(0xFF5AC8FA)
-val StatPinkStart = Color(0xFFFF2D55)
-val StatPinkEnd = Color(0xFFFF6B9D)
+// Premium Stats - 更统一、更高级的统计卡片渐变
+
+// 总曝光：主蓝
+val StatExposureStart = Color(0xFF3F7FB3)
+val StatExposureEnd = Color(0xFF7FB8E6)
+
+// 总点击：浅海蓝
+val StatClickStart = Color(0xFF4A90C9)
+val StatClickEnd = Color(0xFF9CCDEE)
+
+// 总点赞：柔和珊瑚
+val StatLikeStart = Color(0xFFD96B75)
+val StatLikeEnd = Color(0xFFF2A3A8)
+
+// 总收藏：杏仁橙
+val StatFavoriteStart = Color(0xFFD9A441)
+val StatFavoriteEnd = Color(0xFFF3C96E)
+
+// 总分享：薄荷青
+val StatShareStart = Color(0xFF3FAF91)
+val StatShareEnd = Color(0xFF83D7C1)
+
+// CTR：柔和紫蓝
+val StatCtrStart = Color(0xFF7A6FF0)
+val StatCtrEnd = Color(0xFFAFA8FF)
